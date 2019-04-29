77295fd24b92cddff92e774a9237d98deb5520cb
vischugh <vischugh@gmail.com>
2019-04-30 01:01:08 +0200

    Deletions

--- preprocdata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-filename = 'ratings_Baby.csv'
-df = pd.read_csv(filename,names=['userid','itemid','rating','timestamp'])
-df = df.sort_values(['userid','timestamp'],ascending=[True,True])
-df2 = df.groupby('userid').filter(lambda x : len(x) >= 10)
-df2.to_csv('baby_pruned_2.csv')
